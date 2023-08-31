eeadbd03bedca71f96edd7181852a1653dc9d341
“Elodie <mail.elodielongo@gmail.com>
2023-09-01 00:17:28 +0200

    Nuovo file

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-// Your code here
-test
+function greet(name) {
+  name = prompt("What is your name?");
+  alert(`Hello, ${name}!`);
+}
+let age = prompt("How old are you?");
+
+confirm(age <= 17 ? "Did you have birthday?" : "Thanks for your answer.");
