ec0fd101492b722fb0d6d3bc54e6c645bcdc8fd8
ElodieLongo <mail.elodielongo@gmail.com>
2023-09-01 17:11:46 +0200

    Assignment 2

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 function greet() {
-    const name = prompt('What is your name?');
-    alert(name);
+  let name = prompt("What is your name?");
+    alert(`Hello, ${name}!`);
+  let age = prompt("How old are you?");
+  age = parseInt(age);
+
+
+let YesBirthday = confirm("Did you have a birthday yet this year?");
+
+let birthYear;
+
+if (YesBirthday) {
+   birthYear = new Date().getFullYear() - age;
+  } else {
+    birthYear = new Date().getFullYear() - age + 1;
   }
 
-
+  alert(`You were born in ${birthYear}!`);
+}
+  
